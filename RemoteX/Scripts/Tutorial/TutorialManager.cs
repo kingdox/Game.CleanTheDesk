fcaff9d285805ec99700153895c18919b7d26819
kingdox <arpaxavier@gmail.com>
2020-10-24 22:18:38 +0200

    no message

---
+++ RemoteX/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    private readonly Data data = new Data();
+    private DataPass dataPass = null;
+
+
+    [Header("TutorialManager info")]
+    public bool wantInit = true;
+
+
+    private void Start()
+    {
+        dataPass = FindObjectOfType<DataPass>();
+    }
+
+    void Update()
+    {
+        if (dataPass.status == "end")
+        {
+           
+        }
+    }
+
+
+
+
+
+
+
+}
